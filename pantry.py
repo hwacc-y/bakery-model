dc913db506be692f56ffaee03814b45c514af012
Sam Dotson <samgdotson@gmail.com>
2022-01-27 10:50:08 -0600

    moves items to solutions folder

--- pantry.py
+++
@@ -1,9 +0,0 @@
-"""
-This file contains pantry items found in a bakery!
-"""
-
-from pygenesys.commodity.commodity import Commodity
-
-DOUGH = Commodity(comm_name='DOUGH',
-                  units='pounds',
-                  description='tollhouse cookie dough')
